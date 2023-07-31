d92573dc8a76671f9362290537ab9123b1c31fa6
Pavel <web2cap@gmail.com>
2023-07-31 16:42:09 +0200

    api, axios, edit post

--- src/PostPage.js
+++ src/PostPage.js
@@ -12,7 +12,9 @@ const PostPage = ({ posts, handleDelete }) => {
                         <h2>{post.title}</h2>
                         <p className="postDate">{post.datetime}</p>
                         <p className="postBody">{post.body}</p>
-                        <button onClick={() => handleDelete(post.id)}>
+                        <Link to={`/post/edit/${post.id}`}>
+                            <button className="editButton">Edit post</button></Link>
+                        <button className="deleteButton" onClick={() => handleDelete(post.id)}>
                             Delete post
                         </button>
                     </>
